b78016dc5ccda97e591e832bbe64db6a5d03702d
Ping <51146460+PingLLL@users.noreply.github.com>
2019-08-03 01:29:22 +0800

    Update delspace.py
    purpose：delete the left spaces of  the strings to its first byte

--- delspace.py
+++ delspace.py
@@ -1,7 +1,11 @@
+#通过位置变量实现字符串的外部输入
 import sys
 astr = sys.argv[1]
 
+#range('字符串长度') ----> 给每个字符标序号，从而能定位到分片的节点
+#for循环+ if判断 ---->   astr[i] 逐个字符遍历，判断出非空值的节点i
 
 for i in range(len(astr)):
-    if astr[i] == ' ':
-        continue
+    if astr[i] != ' ':
+        break
+print(astr[i:])
