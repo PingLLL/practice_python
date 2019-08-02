b95f0f1084aa950289ae660ab1ae70804e06cd7a
Ping <51146460+PingLLL@users.noreply.github.com>
2019-08-02 07:11:52 -0500

    Create cp.py

---
+++ cp.py
@@ -0,0 +1,12 @@
+#用位置变量调用源文件,目标文件
+import sys
+src = sys.argv[1]
+dest = sys.argv[2]
+
+#为拷贝代码所需的动作定义变量
+opensrc = open(src, 'rb')
+getsrc = opensrc.readlines()
+getdest = open(dest, 'wb')
+
+#真正实现拷贝功能的代码
+getdest.writelines(getsrc)
