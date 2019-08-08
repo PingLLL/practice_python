11c2705968c11b982e8b62c78437d53e94e6fd14
Ping <51146460+PingLLL@users.noreply.github.com>
2019-08-08 08:50:35 -0500

    Create backup.py

---
+++ backup.py
@@ -0,0 +1,14 @@
+import sys
+import tarfile
+
+srcfile = input('source file(path): ')
+destfile = input('destination file(path): ')
+
+
+def full_bak(srcfile, destfile):
+
+
+def increment_bak():
+
+
+if __name__ == '__main__':
